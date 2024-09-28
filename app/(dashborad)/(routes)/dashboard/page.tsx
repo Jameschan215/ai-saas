95522b08f07e784c865e95f8d90e1456a8bf7268
James <26018087+Jameschan215@user.noreply.github.com>
2024-09-28 15:20:03 +0800

    2nd commit - folder structure

---
+++ app/(dashborad)/(routes)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+export default function DashboardPage() {
+	return (
+		<main className="flex items-center justify-center h-full">
+			<p className="text-6xl">
+				Dashboard Page -{' '}
+				<span className="font-bold text-green-700">Protected</span>
+			</p>
+		</main>
+	);
+}
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,10 @@
+export default function LandingPage() {
+	return (
+		<main className="flex items-center justify-center h-full">
+			<p className="text-6xl">
+				Landing Page -{' '}
+				<span className="font-bold text-orange-700">Not protected</span>
+			</p>
+		</main>
+	);
+}
